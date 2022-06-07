2e48729c5866781150e77bdced0a0d268488f040
qbentil <bentilshadrack72@gmail.com>
2022-06-07 10:52:36 +0000

    Fix Responsiveness issue

--- src/components/Header/LoginAction.tsx
+++ src/components/Header/LoginAction.tsx
@@ -3,12 +3,12 @@ import { MdLogin } from "react-icons/md"
 const LoginAction = ({text, action, mobile}:{action:any, text?:string, mobile?:boolean}) => {
   return (
     <motion.div
-    className={` flex items-center gap-3 border border-slate-200 px-3 py-1 rounded-lg`}
+    className={` flex items-center gap-3 border border-slate-200 px-3 py-1 rounded-lg cursor-pointer`}
     whileTap={{ scale: 0.8 }}
     onClick={action}
   >
-    <MdLogin className={`cursor-pointer ${mobile && 'text-3xl text-headingColor'}`} />
-    {text && <p className="text-headingColor cursor-pointer">{text}</p>}
+    <MdLogin className={` ${mobile && 'text-2xl text-headingColor'}`} />
+    {text && <p className="text-headingColor ">{text}</p>}
   </motion.div>
   )
 }
