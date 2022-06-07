04a0809bab0822c8286cb2c850bc92027fdbe7a7
qbentil <bentilshadrack72@gmail.com>
2022-06-07 19:23:35 +0000

    Update App Component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,5 @@
 export {default as Header} from './Header';
 export {default as Home } from './Home';
+export {default as Login} from './Auth/Login';
+export {default as Signup} from './Auth/Register';
 export {Logo} from './Assets';
