7560eba338a6251ab660eed274175f6df14e87fd
qbentil <bentilshadrack72@gmail.com>
2022-06-07 19:22:41 +0000

    Update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Header, Home } from "./components";
+import { Header, Home, Login, Signup } from "./components";
 import { Route, Routes } from "react-router-dom";
 import {AnimatePresence} from "framer-motion";
 function App() {
@@ -7,10 +7,12 @@ function App() {
     <AnimatePresence exitBeforeEnter>
       <div className="w-screen h-auto flex flex-col bg-primary">
         <Header />
-        <main className="mt-14 p-8 w-full h-screen">
+        <main className="mt-14 p-8 w-full h-auto">
           {/* Routes */}
           <Routes>
             <Route path="/*" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Signup />} />
           </Routes>
         </main>
       </div>
