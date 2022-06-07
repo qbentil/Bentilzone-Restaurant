bf1c04a25a03aa23d7e234f9a84af7259433d356
qbentil <bentilshadrack72@gmail.com>
2022-06-07 10:53:44 +0000

    Avoid Mobile Scrool on nav

--- src/components/Header/mobile-nav.tsx
+++ src/components/Header/mobile-nav.tsx
@@ -2,35 +2,43 @@
 import { MdShoppingBasket, MdOutlineRestaurantMenu } from "react-icons/md";
 import { motion } from "framer-motion";
 import { Logo } from "../Assets";
-const MobileNav = ({isOpen, setIsOpen}: {isOpen:boolean, setIsOpen: any}) => {
+import { Link } from "react-router-dom";
+const MobileNav = ({
+  isOpen,
+  setIsOpen,
+}: {
+  isOpen: boolean;
+  setIsOpen: any;
+}) => {
   return (
-    <div className="flex flex-col items-start justify-start gap-16 w-screen h-screen  overflow-y-hidden  z-50 overflow-hidden ">
+    <div className="flex flex-col bg-primary items-start justify-start gap-16 w-screen h-screen  overflow-y-hidden  z-50 overflow-hidden ">
       <motion.div className="flex items-center justify-between w-screen h-24 bg-slate-100 px-10">
-      <motion.div
-        whileTap={{ scale: 0.9 }}
-        initial={{opacity: 0, x:200}}
-        animate={{opacity: 1, x:0}}
-        exit={{opacity: 0, x:200}}
-        className="relative flex items-center justify-center text-textColor"
-      >
-        <MdShoppingBasket className="text-4xl cursor-pointer" />
-        <div className="absolute -top-2 -right-2 w-6 h-6 rounded-full bg-cartNumBg flex items-center justify-center">
-          <p className="text-sm text-white font-semibold">0</p>
-        </div>
-      </motion.div>
-      <motion.div
-        whileTap={{ scale: 0.9 }}
-        initial={{opacity: 0, x:200}}
-        animate={{opacity: 1, x:0}}
-        exit={{opacity: 0, x:200}}
-        className="relative flex items-center justify-center text-textColor"
-        onClick={() => setIsOpen(!isOpen)}
-
-      >
+        <motion.div
+          whileTap={{ scale: 0.9 }}
+          initial={{ opacity: 0, x: 200 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 200 }}
+          className="relative flex items-center justify-center text-textColor"
+        >
+          <MdShoppingBasket className="text-4xl cursor-pointer" />
+          <div className="absolute -top-2 -right-2 w-6 h-6 rounded-full bg-cartNumBg flex items-center justify-center">
+            <p className="text-sm text-white font-semibold">0</p>
+          </div>
+        </motion.div>
+        <motion.div
+          whileTap={{ scale: 0.9 }}
+          initial={{ opacity: 0, x: 200 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 200 }}
+          className="relative flex items-center justify-center text-textColor"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           <MdOutlineRestaurantMenu className="text-headingColor text-4xl" />
+        </motion.div>
       </motion.div>
-      </motion.div>
-      <div className={`flex items-center justify-center w-full  h-72 gap-10 flex-col`}>
+      <div
+        className={`flex items-center justify-center w-full  h-72 gap-10 flex-col`}
+      >
         <p className="text-base text-textColor cursor-pointer hover:text-headingColor duration-100 transition-all ease-in-out px-10">
           Menu
         </p>
@@ -45,16 +53,16 @@ const MobileNav = ({isOpen, setIsOpen}: {isOpen:boolean, setIsOpen: any}) => {
         </p>
       </div>
 
-      <div className="flex items-center  justify-center w-full">
-      <motion.div
-            whileTap={{ scale: 0.9 }}
-            className="flex items-center gap-2 cursor-pointer"
-          >
-            <img src={Logo} alt="Logo" className="w-16 object-cover" />
-            <p className="text-headingColor text-3xl font-bold">Bentilzone</p>
-          </motion.div>
-      </div>
-
+      <Link to={'/'} onClick = {() => setIsOpen(!isOpen)} className="flex items-center  justify-center w-full">
+        <motion.div
+          whileTap={{ scale: 0.9 }}
+          className="flex items-center gap-2 cursor-pointer"
+          
+        >
+          <img src={Logo} alt="Logo" className="w-16 object-cover" />
+          <p className="text-headingColor text-3xl font-bold">Bentilzone</p>
+        </motion.div>
+      </Link>
     </div>
   );
 };
