fdf29e747fd140137a8f3e086d793c9b8bb90316
qbentil <bentilshadrack72@gmail.com>
2022-06-07 19:21:54 +0000

    Update logout action

--- src/components/Header/LoginAction.tsx
+++ src/components/Header/LoginAction.tsx
@@ -1,15 +1,18 @@
 import { motion } from "framer-motion"
 import { MdLogin } from "react-icons/md"
-const LoginAction = ({text, action, mobile}:{action:any, text?:string, mobile?:boolean}) => {
+import { Link } from "react-router-dom"
+const LoginAction = ({text,  mobile}:{text?:string, mobile?:boolean}) => {
   return (
-    <motion.div
-    className={` flex items-center gap-3 border border-slate-200 px-3 py-1 rounded-lg cursor-pointer`}
-    whileTap={{ scale: 0.8 }}
-    onClick={action}
-  >
-    <MdLogin className={` ${mobile && 'text-2xl text-headingColor'}`} />
-    {text && <p className="text-headingColor ">{text}</p>}
-  </motion.div>
+    <Link to="/login">
+        <motion.div
+          className={` flex items-center gap-3 border border-slate-200 px-3 py-1 rounded-lg cursor-pointer`}
+          whileTap={{ scale: 0.8 }}
+          
+        >
+          <MdLogin className={` ${mobile && 'text-2xl text-headingColor'}`} />
+          {text && <p className="text-headingColor ">{text}</p>}
+        </motion.div>
+    </Link>
   )
 }
 
