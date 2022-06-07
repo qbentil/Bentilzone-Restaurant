6a932af28a0eccd4ec83dc85702468aa6db8137d
qbentil <bentilshadrack72@gmail.com>
2022-06-07 21:48:18 +0000

    Add images to Assdrts

--- src/components/Assets/index.tsx
+++ src/components/Assets/index.tsx
@@ -1,5 +1,9 @@
-import LogoI from "../../img/logo.png";
 import AvatarI from "../../img/avatar.png";
+import Cheff1I from "../../img/chef1.png";
+import CheffI from "../../img/cheff.png";
+import LogoI from "../../img/chef1.png";
 
 export const Logo = LogoI
 export const Avatar = AvatarI
+export const Cheff = CheffI
+export const Cheff1 = Cheff1I
