b6244cdfbbfea9f7d28d83df58f1a9962eb1ed92
qbentil <bentilshadrack72@gmail.com>
2022-06-07 10:53:15 +0000

    Fix tablet responsiveness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,5 @@
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { HiOutlineMenuAlt2 } from "react-icons/hi";
-import Avatar from "../../img/avatar.png";
 import { Link } from "react-router-dom";
 import MobileNav from "./mobile-nav";
 import Navigations from "./Navigations";
@@ -11,7 +10,7 @@ import { useStateValue } from "../../context/StateProvider";
 import { useState } from "react";
 import DropDown from "./DropDown";
 import LoginAction from "./LoginAction";
-import { Logo } from "../Assets";
+import { Logo, Avatar } from "../Assets";
 
 const Header = () => {
   const firebaseAuth = getAuth(app);
@@ -45,10 +44,8 @@ const Header = () => {
     }
   };
 
-  return isOpenMobileNav ? (
-    <MobileNav isOpen={isOpenMobileNav} setIsOpen={setIsOpenMobileNav} />
-  ) : (
-    <header className="w-screen fixed z-50   md:p-6 md:px-16">
+  return (
+    <header className="w-screen fixed z-50 bg-primary shadow-xl   md:p-3 md:px-4 lg:p-6 lg:px-16">
       {/* Tablet and Desktop */}
       <div className="hidden md:flex w-full justify-between itesm-center">
         <Link to={"/"}>
@@ -56,8 +53,8 @@ const Header = () => {
             whileTap={{ scale: 0.9 }}
             className="flex items-center gap-2 cursor-pointer"
           >
-            <img src={Logo} alt="Logo" className="w-8 object-cover" />
-            <p className="text-headingColor text-xl font-bold">Bentilzone</p>
+            <img src={Logo} alt="Logo" className="md:w-6 lg:w-8 object-cover" />
+            <p className="text-headingColor md:text-lg lg:text-xl font-bold">Bentilzone</p>
           </motion.div>
         </Link>
 
@@ -84,53 +81,60 @@ const Header = () => {
             <DropDown user={user} action={logout} />
           </div>
         ) : (
-          <LoginAction action={login} text={'Login'} />
+          <LoginAction action={login} text={"Login"} />
         )}
       </div>
 
       {/* Mobile */}
       <motion.div
-        className="flex md:hidden w-full p-5 items-center justify-between"
+        className="flex md:hidden w-full p-0 items-center justify-between"
         initial={{ opacity: 0, x: 200 }}
         animate={{ opacity: 1, x: 0 }}
         exit={{ opacity: 0, x: 200 }}
       >
-        <motion.div
-          whileTap={{ scale: 0.9 }}
-          className=" flex items-center justify-center"
-          onClick={() => setIsOpenMobileNav(!isOpenMobileNav)}
-        >
-          <HiOutlineMenuAlt2 className="text-headingColor text-4xl" />
-        </motion.div>
-        <Link to={"/"}>
-          <motion.div
-            whileTap={{ scale: 0.9 }}
-            className="flex items-center gap-2 cursor-pointer"
-          >
-            <img src={Logo} alt="Logo" className="w-8 object-cover" />
-            <p className="text-headingColor text-xl font-bold">Bentilzone</p>
-          </motion.div>
-        </Link>
-
-        {user ? (
-          <div
-            className={`flex items-center gap-3 border px-3 py-1 rounded-lg relative`}
-          >
+        {isOpenMobileNav ? (
+          <MobileNav isOpen={isOpenMobileNav} setIsOpen={setIsOpenMobileNav} />
+        ) : (
+          <div className="p-5 flex items-center justify-between w-full">
             <motion.div
               whileTap={{ scale: 0.9 }}
-              className="group flex items-center justify-center"
+              className=" flex items-center justify-center"
+              onClick={() => setIsOpenMobileNav(!isOpenMobileNav)}
             >
-              <img
-                src={user?.photoURL ? user.photoURL : Avatar}
-                className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-2xl rounded-full cursor-pointer"
-                alt="user-profile"
-                onClick={() => setIsOpen(!isOpen)}
-              />
-              {isOpen && <DropDown user={user} action={logout} />}
+              <HiOutlineMenuAlt2 className="text-headingColor text-4xl" />
             </motion.div>
+            <Link to={"/"}>
+              <motion.div
+                whileTap={{ scale: 0.9 }}
+                className="flex items-center gap-2 cursor-pointer"
+              >
+                <img src={Logo} alt="Logo" className="w-8 object-cover" />
+                <p className="text-headingColor text-xl font-bold">
+                  Bentilzone
+                </p>
+              </motion.div>
+            </Link>
+            {user ? (
+              <div
+                className={`flex items-center gap-3 border px-3 py-1 rounded-lg relative`}
+              >
+                <motion.div
+                  whileTap={{ scale: 0.9 }}
+                  className="group flex items-center justify-center"
+                >
+                  <img
+                    src={user?.photoURL ? user.photoURL : Avatar}
+                    className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-2xl rounded-full cursor-pointer"
+                    alt="user-profile"
+                    onClick={() => setIsOpen(!isOpen)}
+                  />
+                  {isOpen && <DropDown user={user} action={logout} />}
+                </motion.div>
+              </div>
+            ) : (
+              <LoginAction action={login} mobile />
+            )}
           </div>
-        ) : (
-          <LoginAction action={login} mobile />
         )}
       </motion.div>
     </header>
