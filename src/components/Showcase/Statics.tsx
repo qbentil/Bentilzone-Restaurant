c7f3e1c6f3533f079e010bad1e88766422972094
qbentil <bentilshadrack72@gmail.com>
2022-06-08 09:04:49 +0000

    Responsive SHowcase Static food

--- src/components/Showcase/Statics.tsx
+++ src/components/Showcase/Statics.tsx
@@ -8,20 +8,21 @@ interface Props {
 
 const StaticsImages: React.FC<Props> = ({ data }) => {
   return (
-    <div className="w-full h-full absolute flex items-center justify-center mt-6 md:mt-0 top-6 left-0 px-32 py-4 gap-20 md:gap-4 flex-wrap ">
+    <div className="w-full h-full absolute flex items-center justify-center top-6 left-0 lg:px-28 lg:py-4 gap-4 flex-wrap ">
       {data.map((item) => (
         <div
           key={item.id}
-          className="cursor-pointer min-w-[190px] drop-shadow-lg p-2 bg-cardOverlay backdrop-blur-md rounded-xl flex flex-col items-center justify-center"
+          className="cursor-pointer min-h-[140px] lg:min-h-[220px] min-w-[160px] lg:min-w-[200px] drop-shadow-lg p-2 bg-cardOverlay backdrop-blur-md rounded-xl flex flex-col items-center justify-center"
         >
           <motion.img
             whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 1.1 }}
             src={item.imgSrc}
             alt="icecream"
-            className="w-40 -mt-20"
+            className="w-24 lg:w-40 -mt-10 lg:-mt-20"
           />
-          <p className="text-lg font-semibold text-textColor">{item.title}</p>
-          <p className="text-md text-lightGray font-semibold my-3">
+          <p className="text-base lg:text-lg font-semibold text-textColor">{item.title}</p>
+          <p className="text-[10px] lg:text-lg text-lightGray font-semibold my-2 lg:my-3">
             {item.desc}
           </p>
           <p className="text-sm font-semibold text-headingColor">
