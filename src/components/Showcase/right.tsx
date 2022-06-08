0f80e985d24899ea4123a7bca1fe99624c8658dd
qbentil <bentilshadrack72@gmail.com>
2022-06-08 02:25:06 +0000

    Showcase Right Componet

---
+++ src/components/Showcase/right.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { HeroBg } from '../Assets'
+import StaticsImages from './Statics'
+import { data } from '../../utils/showcaseStatic'
+const Right = () => {
+  return (
+    <div className="py-2 flex-1 flex items-center relative">
+      <img src={HeroBg} alt="" className='ml-auto lg:h-[550px] h-[420px] w-full lg:w-auto' />
+      <StaticsImages data = {data} />
+    </div>
+  )
+}
+
+export default Right
