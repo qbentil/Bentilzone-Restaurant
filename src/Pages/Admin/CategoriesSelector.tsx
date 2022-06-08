4a12a296d0be772c975d0f2d68168de925dbb4da
qbentil <bentilshadrack72@gmail.com>
2022-06-08 16:40:11 +0000

    Categories Selector Component for Add Product form

---
+++ src/Pages/Admin/CategoriesSelector.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+export type FoodCategory = {
+  id: number;
+  name: string;
+  urlParam: string;
+};
+
+export type FoodCategories = FoodCategory[];
+
+interface Props {
+  categories: FoodCategories;
+  action: any;
+  selected: string;
+}
+const CategoriesSelector: React.FC<Props> = ({
+  categories,
+  action,
+  selected,
+}) => {
+  return (
+    <select name="categories" id="categories" className="outline-none w-full capitalize text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer" onChange={(e) => action(e.target.value)}>
+      <option defaultValue={selected}  className="bg-white capitalize">
+        {selected}
+      </option>
+      {categories
+        .filter((cat) => cat.urlParam !== selected)
+        .map((category) => (
+          <option key={category.id} value={category.urlParam} className = "text-base border-0 outline-none uppercase bg-white text-headingColor">
+            {category.name}
+          </option>
+        ))}
+    </select>
+  );
+};
+
+export default CategoriesSelector;
