236693ad32a06a0de5ef7ae991871d11c03dfb50
qbentil <bentilshadrack72@gmail.com>
2022-06-08 02:27:25 +0000

    Add Assets Imports

--- src/components/Assets/index.tsx
+++ src/components/Assets/index.tsx
@@ -2,8 +2,25 @@ import AvatarI from "../../img/avatar.png";
 import Cheff1I from "../../img/chef1.png";
 import CheffI from "../../img/cheff.png";
 import LogoI from "../../img/chef1.png";
+import BikeDeliveryI from "../../img/delivery.png";
+import HeroBgI from "../../img/hero-bg.png";
 
+// Showcase Banner Static Assets
+import IcreamI from "../../img/i1.png";
+import StrawberryI from "../../img/f5.png";
+import ChickenI from "../../img/c3.png";
+import FishI from "../../img/fi3.png";
+
+
+
+// Exports
 export const Logo = LogoI
 export const Avatar = AvatarI
 export const Cheff = CheffI
 export const Cheff1 = Cheff1I
+export const BikeDelivery = BikeDeliveryI
+export const HeroBg = HeroBgI
+export const Icecream = IcreamI
+export const Strawberry = StrawberryI
+export const Chicken = ChickenI
+export const Fish = FishI
