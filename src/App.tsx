1a1b1cc59f08fc04ece1673172c5673ea08b0cbe
qbentil <bentilshadrack72@gmail.com>
2022-06-08 02:27:05 +0000

    Update Root App

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ import {AnimatePresence} from "framer-motion";
 function App() {
   return (
     <AnimatePresence exitBeforeEnter>
-      <div className="w-screen h-auto min-h-[100vh] flex flex-col">
+      <div className="w-screen h-auto min-h-[100vh] flex flex-col bg-primary">
         <Header />
-        <main className="mt-14 p-8 w-full h-auto">
+        <main className="mt-16 md:mt-16 px-3 md:px-8 md:py-6 py-4 w-full h-auto">
           {/* Routes */}
           <Routes>
             <Route path="/*" element={<Home />} />
