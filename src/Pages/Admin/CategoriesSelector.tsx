530916c20f8fffc28d71f63a7ac49b08afc4a7a8
qbentil <bentilshadrack72@gmail.com>
2022-06-09 04:59:44 +0000

    Update Add Caegories Component

--- src/Pages/Admin/CategoriesSelector.tsx
+++ src/Pages/Admin/CategoriesSelector.tsx
@@ -18,7 +18,7 @@ const CategoriesSelector: React.FC<Props> = ({
   selected,
 }) => {
   return (
-    <select name="categories" id="categories" className="outline-none w-full capitalize text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer" onChange={(e) => action(e.target.value)}>
+    <select name="categories" id="categories" className="bg-transparent outline-none w-full capitalize text-base border-2 border-gray-200 p-2 rounded-md cursor-pointer" onChange={(e) => action(e.target.value)}>
       <option defaultValue={selected? selected: "Select category"}  className="bg-white capitalize">
         {selected? selected: "Select category"}
       </option>
