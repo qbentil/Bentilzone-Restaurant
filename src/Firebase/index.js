bbbdffe5a91a1446f272bac8b1ab5c929285a834
qbentil <bentilshadrack72@gmail.com>
2022-06-09 04:58:22 +0000

    ADD Auth Methods to Firbase Functions

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -1,14 +1,21 @@
-import { getDownloadURL, ref, uploadBytesResumable, deleteObject } from "firebase/storage";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import {
+  getDownloadURL,
+  ref,
+  uploadBytesResumable,
+  deleteObject,
+} from "firebase/storage";
 import React from "react";
 import { MdOutlineCloudUpload } from "react-icons/md";
 import { toast } from "react-toastify";
-import { storage } from "../firebase.config";
+import { useStateValue } from "../context/StateProvider";
+import { app, firestore, storage } from "../firebase.config";
 
 export const firebaseUploadImage = (
   imageFile,
   promise,
   progressHandler,
-  errorHandler,
   action
 ) => {
   promise(true);
@@ -25,13 +32,15 @@ export const firebaseUploadImage = (
     "state_changed",
     (snapshot) => {
       progressHandler(
-        `Upload status: ${Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)}%`
+        `Upload status: ${Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        )}%`
       );
     },
     (error) => {
       console.log(error);
       toast.error("Error while uploading, Try again🤗");
-      errorHandler(true);
+      action(null);
       setTimeout(() => {
         promise(false);
       }, 3000);
@@ -46,16 +55,46 @@ export const firebaseUploadImage = (
   );
 };
 
-export const firebaseRemoveUploadedImage = (ImageFile, imageHandler, promise) => {
-  promise(true)
+export const firebaseRemoveUploadedImage = (
+  ImageFile,
+  imageHandler,
+  promise
+) => {
+  promise(true);
   toast.info(`Removing Image.....`, {
     icon: <MdOutlineCloudUpload className="text-blue-600" />,
-    autoClose: 1500
+    autoClose: 1500,
   });
   const deleteRef = ref(storage, ImageFile);
   deleteObject(deleteRef).then(() => {
-    imageHandler(null)
-    promise(false)
-    toast.success("Photo removed Successfully😊", {autoClose: 2000});
-  })
-}
+    imageHandler(null);
+    promise(false);
+    toast.success("Photo removed Successfully😊", { autoClose: 2000 });
+  });
+};
+export const silentRemoveUploadedImage = (ImageFile) => {
+  const deleteRef = ref(storage, ImageFile);
+  deleteObject(deleteRef).then(() => {});
+};
+
+export const firebaseSaveProduct = async (data) => {
+  await setDoc(doc(firestore, "Food", `${Date.now()}`), data, {
+    merge: true,
+  });
+};
+
+export const AUTHPROVIDER = async (provider) => {
+  const firebaseAuth = getAuth(app);
+  const {
+    user: { refreshToken, providerData },
+  } = await signInWithPopup(firebaseAuth, provider);
+  return { refreshToken, providerData };
+};
+export const EMAILSIGNUP = async (email, password) => {
+  const firebaseAuth = getAuth(app);
+  return createUserWithEmailAndPassword(firebaseAuth, email, password)
+};
+export const EMAILSIGNIN = async (email, password) => {
+  const firebaseAuth = getAuth(app);
+  return signInWithEmailAndPassword(firebaseAuth, email, password)
+};
