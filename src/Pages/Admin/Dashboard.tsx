e8a78780eee7035e5e4fc714f699d19632b53409
qbentil <bentilshadrack72@gmail.com>
2022-06-09 22:22:33 +0000

    FetchFoodData on each item saved

--- src/Pages/Admin/Dashboard.tsx
+++ src/Pages/Admin/Dashboard.tsx
@@ -1,15 +1,25 @@
-
 import { toast } from "react-toastify";
 
 import { AssetUploader, Loader } from "../../components";
-import {   MdDeleteOutline,  MdOutlineDataSaverOn,  MdOutlineFastfood, MdOutlineFoodBank, MdOutlineProductionQuantityLimits } from "react-icons/md";
-import {GiTakeMyMoney} from "react-icons/gi";
+import {
+  MdDeleteOutline,
+  MdOutlineDataSaverOn,
+  MdOutlineFastfood,
+  MdOutlineFoodBank,
+  MdOutlineProductionQuantityLimits,
+} from "react-icons/md";
+import { GiTakeMyMoney } from "react-icons/gi";
 import { BiCategory, BiFoodMenu } from "react-icons/bi";
 import { Categories } from "../../utils/categories";
 import CategoriesSelector from "./CategoriesSelector";
 import { useState } from "react";
 import { motion } from "framer-motion";
-import { firebaseRemoveUploadedImage, firebaseSaveProduct } from "../../Firebase";
+import {
+  firebaseFetchFoodItems,
+  firebaseRemoveUploadedImage,
+  firebaseSaveProduct,
+} from "../../Firebase";
+import { useStateValue } from "../../context/StateProvider";
 
 const Dashboard = () => {
   const [title, setTitle] = useState("");
@@ -20,21 +30,22 @@ const Dashboard = () => {
   const [loading, setLoading] = useState(false);
   const [quantity, setQuantity] = useState("");
   const [description, setDescription] = useState("");
-  const [loaderMessage, setLoadermessage] = useState("")
+  const [loaderMessage, setLoadermessage] = useState("");
+  const [{foodItems}, dispatch] = useStateValue();
 
   const deleteImage = () => {
-    setLoadermessage("Removing Photo......")
-    firebaseRemoveUploadedImage(image, setImage, setLoading)
+    setLoadermessage("Removing Photo......");
+    firebaseRemoveUploadedImage(image, setImage, setLoading);
   };
   const saveItem = () => {
-    setLoadermessage(`Saving Product ${title}.`)
-    setLoading(true)
+    setLoadermessage(`Saving Product ${title}.`);
+    setLoading(true);
     try {
-      if(!title || !calories || !price || !image || !category) {
-        toast.error("Please fill all fields before saving product 🤗")
-        setLoading(false)
-        return
-      }else{
+      if (!title || !calories || !price || !image || !category) {
+        toast.error("Please fill all fields before saving product 🤗");
+        setLoading(false);
+        return;
+      } else {
         const data = {
           id: Date.now(),
           title: title,
@@ -44,29 +55,29 @@ const Dashboard = () => {
           price: price,
           imageURL: image,
           qty: quantity,
-        }
-        toast.promise(firebaseSaveProduct(data), {
+        };
+        toast
+          .promise(firebaseSaveProduct(data), {
             pending: "Saving Product...",
             success: "Product saved successfully",
             error: "Error saving product, Please try again🤗",
-        }).then(() => {
-          clearForm();
-          setLoading(false)
-        }).catch((error) => {
-          console.log(error)
-        })
-        setLoadermessage("")
-        setLoading(false)
-
-
-
+          })
+          .then(() => {
+            clearForm();
+            setLoading(false);
+            fetchData();
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        setLoadermessage("");
+        setLoading(false);
       }
-
     } catch (error) {
-      console.log(error);      
-      toast.error("Error whilesaving product")
+      console.log(error);
+      toast.error("Error whilesaving product");
     }
-  }
+  };
   const clearForm = () => {
     setTitle("");
     setCalories("");
@@ -75,22 +86,29 @@ const Dashboard = () => {
     // setCategory("");
     setQuantity("");
     setDescription("");
+  };
 
-  }
-
-  const validateNumber = (value:any) => {
+  const validateNumber = (value: any) => {
     if (isNaN(value)) {
-      toast.error("Please enter a valid number", {toastId: 123})
+      toast.error("Please enter a valid number", { toastId: 123 });
       return "";
     }
     return value;
+  };
+
+
+  const fetchData = async () => {
+    await firebaseFetchFoodItems().then((data) => {
+      dispatch({
+        type: "SET_FOOD_ITEMS",
+        foodItems: data,
+      });
+    })
   }
+
   return (
     <div className="w-full min-h-[80vh] flex items-center justify-center mt-5">
-      <div
-        // onClick={() => notify()}
-        className="border w-[90%] md:w-[75%]  flex border-gray-300 items-center rounded-lg p-4 flex-col justify-center gap-4  "
-      >
+      <div className="border w-[90%] md:w-[75%]  flex border-gray-300 items-center rounded-lg p-4 flex-col justify-center gap-4  ">
         <div className="w-full py-3 border-b border-gray-300 flex -tems-center gap-2">
           <MdOutlineFastfood className="text-xl text-gray-600" />
           <input
@@ -106,12 +124,12 @@ const Dashboard = () => {
 
         <div className="w-full flex flex-col md:flex-row items-center gap-3">
           <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
-          <BiCategory className="text-xl text-gray-600" />
-          <CategoriesSelector
-            categories={Categories}
-            action={setCategory}
-            selected={category}
-          />
+            <BiCategory className="text-xl text-gray-600" />
+            <CategoriesSelector
+              categories={Categories}
+              action={setCategory}
+              selected={category}
+            />
           </div>
           <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
             <MdOutlineProductionQuantityLimits className="text-gray-600 text-2xl" />
@@ -128,7 +146,7 @@ const Dashboard = () => {
         </div>
         <div className="group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-[225px]  md:h-[420px] round-lg">
           {loading ? (
-            <Loader progress = {loaderMessage} />
+            <Loader progress={loaderMessage} />
           ) : (
             <>
               {image ? (
@@ -140,7 +158,7 @@ const Dashboard = () => {
                       className="w-full h-full object-cover"
                     />
                     <button
-                    title="Remove Photo"
+                      title="Remove Photo"
                       className="absolute bottom-3 right-3 rounded-full p-2 md:p-5 bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md duration-500 transition-all ease-in-out"
                       onClick={() => deleteImage()}
                     >
@@ -149,7 +167,11 @@ const Dashboard = () => {
                   </div>
                 </>
               ) : (
-                <AssetUploader action = {setImage} progressHandler = {setLoadermessage} promise = {setLoading}/>
+                <AssetUploader
+                  action={setImage}
+                  progressHandler={setLoadermessage}
+                  promise={setLoading}
+                />
               )}
             </>
           )}
@@ -194,8 +216,12 @@ const Dashboard = () => {
         </div>
 
         <div className="w-full flex items-center justify-center">
-          <motion.button whileHover={{scale:1.1}} className="ml-0 flex justify-center items-center gap-2 flex-row-reverse md:ml-auto w-full md:w-auto border-none outline-none rounded bg-orange-500 px-12 py-2 text-lg text-white" onClick={() => saveItem()}>
-           <MdOutlineDataSaverOn /> Save
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            className="ml-0 flex justify-center items-center gap-2 flex-row-reverse md:ml-auto w-full md:w-auto border-none outline-none rounded bg-orange-500 px-12 py-2 text-lg text-white"
+            onClick={() => saveItem()}
+          >
+            <MdOutlineDataSaverOn /> Save
           </motion.button>
         </div>
       </div>
