9e09cdf2800a757bec1f66ac790323a7839e6e96
qbentil <bentilshadrack72@gmail.com>
2022-06-10 14:40:59 +0000

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         textColor: "#515151",
         cardOverlay: "rgba(256, 256, 256, 0.4)",
         lightGray: "#9ca0ab",
+        containerbg: "rgba(255, 131, 0, 0.04)",
       },
       display: ["group-hover"]
     },
