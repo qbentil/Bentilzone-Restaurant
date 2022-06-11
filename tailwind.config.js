51d0e9fc3bf51c18d0ee59394434b8b3f58d0c08
qbentil <bentilshadrack72@gmail.com>
2022-06-11 00:43:13 +0000

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
         cartNumBg: "#e80013",
         textColor: "#515151",
         cardOverlay: "rgba(256, 256, 256, 0.4)",
+        btnOverlay: "rgba(255, 255, 255, 0.8)",
         lightGray: "#9ca0ab",
         containerbg: "rgba(255, 131, 0, 0.04)",
       },
