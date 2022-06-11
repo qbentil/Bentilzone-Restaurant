c62d688517e3b1bd4a98bc764e73a8212830cd85
qbentil <bentilshadrack72@gmail.com>
2022-06-11 01:42:25 +0000

    Export New Asset

--- src/components/Assets/index.tsx
+++ src/components/Assets/index.tsx
@@ -5,7 +5,7 @@ import LogoI from "../../img/chef1.png";
 import BikeDeliveryI from "../../img/delivery.png";
 import HeroBgI from "../../img/hero-bg.png";
 import EmptyCartI from "../../img/emptyCart.svg";
-
+import NotFoundI from "../../img/NotFound.svg";
 // Showcase Banner Static Assets
 import IcreamI from "../../img/i1.png";
 import StrawberryI from "../../img/f5.png";
@@ -26,3 +26,4 @@ export const Strawberry = StrawberryI
 export const Chicken = ChickenI
 export const Fish = FishI
 export const EmptyCartImg = EmptyCartI
+export const NotFoundImg = NotFoundI
