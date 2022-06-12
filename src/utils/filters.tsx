ba6bead78d500958755cf2fc8096a3b4815a8c27
qbentil <bentilshadrack72@gmail.com>
2022-06-12 02:24:15 +0000

    Add Cart Item Filter by user

--- src/utils/filters.tsx
+++ src/utils/filters.tsx
@@ -5,3 +5,8 @@ export const FilterFood = (category:string) => {
     const [{foodItems}, dispatch] = useStateValue()
     return foodItems?.filter((item:FoodItem) => item.category.toLowerCase() === category.toLowerCase())
 }
+
+export const GetFoodById = (id: number) => {
+    const [{foodItems}, dispatch] = useStateValue()
+    return foodItems?.find((item:FoodItem) => item.id === id)
+}
