2b22f70dec530149a7a9b737d8f14b75faf99f3d
qbentil <bentilshadrack72@gmail.com>
2022-06-12 03:03:00 +0000

    Add Delete Cart Item Function

--- src/utils/functions.tsx
+++ src/utils/functions.tsx
@@ -1,5 +1,10 @@
 import { cartItem, FoodItem } from "../../types";
-import { firebaseFetchAllCartItems, firebaseFetchFoodItems, firebaseUpdateCartItem } from "../Firebase";
+import {
+  firebaseDeleteCartItem,
+  firebaseFetchAllCartItems,
+  firebaseFetchFoodItems,
+  firebaseUpdateCartItem,
+} from "../Firebase";
 
 export const dispatchtUserCartItems = (
   uid: string,
@@ -17,49 +22,124 @@ export const dispatchtUserCartItems = (
 
 export const fetchUserCartData = async (user: any, dispatch: any) => {
   if (user) {
-    await firebaseFetchAllCartItems().then((data) => {
-      const userCart = dispatchtUserCartItems(user.uid, data, dispatch);
-      localStorage.setItem("cartItems", JSON.stringify(userCart));
-    });
+    await firebaseFetchAllCartItems()
+      .then((data) => {
+        const userCart = dispatchtUserCartItems(user.uid, data, dispatch);
+        localStorage.setItem("cartItems", JSON.stringify(userCart));
+      })
+      .then(() => {})
+      .catch((e) => {
+        console.log(e);
+      });
   } else {
     localStorage.setItem("cartItems", "undefined");
   }
 };
 
 export const fetchFoodData = async (dispatch: any) => {
-  await firebaseFetchFoodItems().then((data) => {
-    dispatch({
-      type: "SET_FOOD_ITEMS",
-      foodItems: data,
+  await firebaseFetchFoodItems()
+    .then((data) => {
+      dispatch({
+        type: "SET_FOOD_ITEMS",
+        foodItems: data,
+      });
+    })
+    .then(() => {})
+    .catch((e) => {
+      console.log(e);
     });
-  });
 };
 
+export const getFoodyById = (menu: FoodItem[], fid: number) => {
+  return menu.find((item: FoodItem) => item.id === fid);
+};
 
-export const getFoodyById = (menu:FoodItem[], fid: number) => {
-    return menu.find((item: FoodItem) => item.id === fid);
-}
+//  Update cart item State
+export const updateCartItemState = async (
+  cartItems: cartItem[],
+  item: cartItem,
+  dispatch: any
+) => {
+  const index = cartItems.findIndex(
+    (cartItem: cartItem) => cartItem.id === item.id
+  );
+  if (index !== -1) {
+    cartItems[index] = item;
+  }
+  dispatch({
+    type: "SET_CARTITEMS",
+    cartItems: cartItems,
+  });
+  await firebaseUpdateCartItem(item)
+    .then(() => {})
+    .catch((e) => {
+      console.log(e);
+    });
+};
 
-export const updateCartItemState  = async (cartItems: cartItem[], item: cartItem, dispatch:any) => {
-    const index = cartItems.findIndex((cartItem: cartItem) => cartItem.id === item.id);
-    if (index !== -1) {
-        cartItems[index] = item;
-    }
+// Update Cart Item Quantity
+export const updateCartItemQty = async (
+  cartItems: cartItem[],
+  item: cartItem,
+  dispatch: any,
+  val: number
+) => {
+  const index = cartItems.findIndex(
+    (cartItem: cartItem) => cartItem.id === item.id
+  );
+  if (index !== -1) {
+    cartItems[index].qty += val;
     dispatch({
-        type: "SET_CARTITEMS",
-        cartItems: cartItems,
-    })
-    await firebaseUpdateCartItem(item);
-}
+      type: "SET_CARTITEMS",
+      cartItems: cartItems,
+    });
+    await firebaseUpdateCartItem(cartItems[index])
+      .then(() => {})
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+};
 
-export const updateCartItemQty = async (cartItems: cartItem[], item: cartItem, dispatch:any, val:number) => {
-    const index = cartItems.findIndex((cartItem: cartItem) => cartItem.id === item.id);
-    if (index !== -1) {
-        cartItems[index].qty += val;
-    }
+//  Delete Cart Item
+export const deleteCartItem = async (
+  cartItems: cartItem[],
+  item: cartItem,
+  dispatch: any
+) => {
+  const index = cartItems.findIndex(
+    (cartItem: cartItem) => cartItem.id === item.id
+  );
+  if (index !== -1) {
+    cartItems.splice(index, 1);
     dispatch({
-        type: "SET_CARTITEMS",
-        cartItems: cartItems,
-    })
-    await firebaseUpdateCartItem(cartItems[index]);
-}
+      type: "SET_CARTITEMS",
+      cartItems: cartItems,
+    });
+    await firebaseDeleteCartItem(item)
+      .then(() => {})
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+};
+
+export const shuffleItems = (items: any) => {
+  let currentIndex = items.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex !== 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [items[currentIndex], items[randomIndex]] = [
+      items[randomIndex],
+      items[currentIndex],
+    ];
+  }
+
+  return items;
+};
