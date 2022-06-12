978486db0b12ce3e30d8a2b0c72f81e6237b6384
qbentil <bentilshadrack72@gmail.com>
2022-06-12 02:24:48 +0000

    Update Cart body with dynamic Cart

--- src/components/Cart/Body.tsx
+++ src/components/Cart/Body.tsx
@@ -1,14 +1,17 @@
 import CartItem from './Item'
 import CartTotal from './CartTotal'
+import { useStateValue } from '../../context/StateProvider';
 
 const CartBody = () => {
+  const [{cartItems}, dispatch] = useStateValue();
   return (
     <div className='w-full h-full rounded-t-[2rem]  bg-cartBg flex flex-col'>
         <div className='w-full h-[340px] md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll scrollbar-hidden'>
-          <CartItem />
-          <CartItem />
-          <CartItem />
-          <CartItem />
+        {
+          cartItems && cartItems.length > 0 && cartItems.map((item:any) => {
+            return <CartItem key={item.id} item={item} />
+          } )
+        }
         </div>
         <CartTotal />
     </div>
