95eb0184720d22cdaedde122bab545c935df454a
qbentil <bentilshadrack72@gmail.com>
2022-06-20 05:21:29 +0000

    Export Newly created COmponents

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -8,3 +8,7 @@ export {default as MenuSection} from './Sections/Menu';
 export {default as EmptyCart} from './EmptyCart';
 export {default as Cart} from './Cart';
 export {default as Footer} from './Footer';
+export {default as Sidenav} from './Admin/Sidenav';
+export {default as Content} from './Admin/Content';
+export {default as AddFood} from './Admin/AddFood';
+export {default as Stats} from './Admin/Dashboard';
