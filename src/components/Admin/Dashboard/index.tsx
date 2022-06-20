fb4f59b9367cba3218b76cd8d47fd74d265719ed
qbentil <bentilshadrack72@gmail.com>
2022-06-20 05:23:56 +0000

    ✨: Create Dashboard Component

---
+++ src/components/Admin/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='w-full h-full items-center justify-center flex'>Dashboard</div>
+  )
+}
+
+export default Dashboard
