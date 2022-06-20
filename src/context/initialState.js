8e84d95f38ed80fcc33b37fd8c6c6069d9b7c0da
qbentil <bentilshadrack72@gmail.com>
2022-06-20 05:25:34 +0000

    Set Initial State value for usermode

--- src/context/initialState.js
+++ src/context/initialState.js
@@ -1,11 +1,12 @@
-import { fetchSessionUser } from "../utils/fetchSessionData";
+import { fetchSessionUser, fetchSessionUserMode } from "../utils/fetchSessionData";
 
 const sessionUser = fetchSessionUser();
-
+const sessionUserMode = fetchSessionUserMode();
 export const initialState = {
     user: sessionUser,
     foodItems: null,
     showCart: false,
     cartItems: [],
     cartTotal: 0,
+    adminMode: sessionUserMode,
 }
