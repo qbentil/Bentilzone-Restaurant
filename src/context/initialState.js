9b01da527f37be0fa6e331af51c0619572406944
qbentil <bentilshadrack72@gmail.com>
2022-06-21 01:24:41 +0000

    Initial state of users

--- src/context/initialState.js
+++ src/context/initialState.js
@@ -1,7 +1,9 @@
 import { fetchSessionUser, fetchSessionUserMode } from "../utils/fetchSessionData";
+import { getAllUser } from "../utils/functions";
 
 const sessionUser = fetchSessionUser();
 const sessionUserMode = fetchSessionUserMode();
+const users = getAllUser()
 export const initialState = {
     user: sessionUser,
     foodItems: null,
@@ -9,4 +11,5 @@ export const initialState = {
     cartItems: [],
     cartTotal: 0,
     adminMode: sessionUserMode,
+    users: users,
 }
