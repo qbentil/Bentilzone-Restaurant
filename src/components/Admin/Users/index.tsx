b21e62c18479d776583dfd96048d3c3eea0cab01
qbentil <bentilshadrack72@gmail.com>
2022-06-21 01:25:18 +0000

    Render user items on Users page

--- src/components/Admin/Users/index.tsx
+++ src/components/Admin/Users/index.tsx
@@ -1,43 +1,45 @@
-import React from "react";
+import { useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+import { useStateValue } from "../../../context/StateProvider";
+import User from "./user";
+
 
 const Users = () => {
+  const [{ users }, dispatch] = useStateValue();
+  useEffect(() => {
+    console.log(users)
+  }, [users])
   return (
-    <div className="w-full h-full border flex border-gray-300  rounded-lg p-4 flex-col gap-4">
-      <table className="divide-y divide-gray-300 w-full" id="dataTable">
-        <thead className="bg-orange-600 text-white">
-          <tr>
-            <th className="px-6 py-2 text-xs">ID</th>
-            <th className="px-6 py-2 text-xs">Photo</th>
-            <th className="px-6 py-2 text-xs">Email</th>
-            <th className="px-6 py-2 text-xs">Created_at</th>
-            <th className="px-6 py-2 text-xs">Action</th>
-          </tr>
-        </thead>
-        <tbody className="bg-white divide-y divide-gray-500">
-          <tr className="whitespace-nowrap">
-            <td className="px-6 py-4 text-sm text-center text-gray-500">1</td>
-            <td className="px-6 py-4 text-center">
-              <div className="text-sm text-gray-900">Jon doe</div>
-            </td>
-            <td className="px-6 py-4 text-center">
-              <div className="text-sm text-gray-500">jhondoe@example.com</div>
-            </td>
-            <td className="px-6 py-4 text-sm text-center text-gray-500">
-              2021-1-12
-            </td>
-            <td className="px-6 py-4 text-center flex gap-3 items-center justify-center">
-              <p className="px-4 py-1 text-sm text-blue-600 bg-blue-200 rounded-full">
-                Edit
-              </p>
-              <p className="px-4 py-1 text-sm text-red-400 bg-red-200 rounded-full">
-                Delete
-              </p>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <div className="w-full justify-center flex flex-col">
+      {/* search bar */}
+      <div className="w-full flex justify-center p-2 bg-white mb-4 rounded-lg">
+        <input
+          className="w-full p-2 outline-none rounded-lg "
+          type="text"
+          placeholder="Search user"
+        />
+        {/* search button */}
+        <button className="flex items-center justify-center gap-3 text-orange-700 font-bold py-2 px-4 rounded-lg">
+          <FaSearch />
+        </button>
+      </div>
+
+      {/* dasboard statistics and counts */}
+      <div className="w-full grid grid-cols-3 gap-1">
+        <User />
+        <User />
+        <User />
+        <User />
+        <User />
+        <User />
+        <User />
+        <User />
+        <User />
+      </div>
     </div>
   );
 };
 
 export default Users;
+
+
