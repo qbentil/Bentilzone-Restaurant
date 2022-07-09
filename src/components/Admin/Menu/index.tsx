5ac1a085f011b86d5e256f6616768ad17b827f8c
qbentil <bentilshadrack72@gmail.com>
2022-07-09 17:50:50 +0000

    Query food Function

--- src/components/Admin/Menu/index.tsx
+++ src/components/Admin/Menu/index.tsx
@@ -1,10 +1,22 @@
 import { FaSearch } from "react-icons/fa";
 import { FoodItem } from "../../../../types";
-import { useStateValue } from "../../../context/StateProvider";
 import { SingleFoodItem } from "../../FoodItem";
+import React, { useState } from "react";
+import { useStateValue } from "../../../context/StateProvider";
 
 const Menu = () => {
     const [{ foodItems }, dispatch] = useStateValue();
+    const [query, setQuery] = useState("");
+    const [filteredFoodItems, setFilteredFoodItems] = useState<FoodItem[]>(foodItems);
+    
+    const filterFood = (e:React.ChangeEvent<HTMLInputElement>) => {
+        if(e.target.value === "") {
+            setFilteredFoodItems(foodItems);
+        }else{
+          const filteredFoodItems = foodItems.filter((foodItem:FoodItem) => foodItem.title.toLowerCase().includes(e.target.value.toLowerCase()));
+          setFilteredFoodItems(filteredFoodItems);
+        }
+    }
   return (
     <div className="w-full flex flex-col justify-center">
       {/* search bar */}
@@ -13,6 +25,8 @@ const Menu = () => {
           className="w-full p-2 outline-none rounded-lg "
           type="text"
           placeholder="Search food"
+          value={query}
+          onChange={(e) => filterFood(e)}
         />
         {/* search button */}
         <button className="flex items-center justify-center gap-3 text-orange-700 font-bold py-2 px-4 rounded-lg">
@@ -21,7 +35,7 @@ const Menu = () => {
       </div>
       <div className="w-full flex items-center justify-center gap-3 overflow-x-hidden flex-wrap">
         {
-            foodItems.map((item: FoodItem) => (
+            filteredFoodItems.map((item: FoodItem) => (
                 <SingleFoodItem key={item.id} item={item} col admin />
             ))
         }
