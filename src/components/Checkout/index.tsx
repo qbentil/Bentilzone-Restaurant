832743245cb8ee5ba1a7525259da6640a58ccb15
qbentil <bentilshadrack72@gmail.com>
2022-07-10 17:15:56 +0000

    Setup Checkout pane

---
+++ src/components/Checkout/index.tsx
@@ -0,0 +1,16 @@
+import { motion } from "framer-motion";
+import Header from "./Header";
+const Checkout = ({handler}: {handler: any}) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: 200 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: 200 }}
+      className="w-full h-screen md:w-[350px] bg-white md:backdrop-blur-sm flex flex-col z-[101] drop-shadow-xl fixed top-0 right-0"
+    >
+        <Header action={handler} />
+    </motion.div>
+  );
+};
+
+export default Checkout;
