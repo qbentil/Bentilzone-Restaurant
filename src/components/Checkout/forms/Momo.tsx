ccc112032c21a912fd879e32b971ba0470d331d5
qbentil <bentilshadrack72@gmail.com>
2022-07-17 11:09:14 +0000

    COllect Payment Data on input

--- src/components/Checkout/forms/Momo.tsx
+++ src/components/Checkout/forms/Momo.tsx
@@ -1,23 +1,34 @@
-import React from 'react'
+import { useStateValue } from "../../../context/StateProvider";
 
 const MomoForm = () => {
+  const [{checkoutData}, dispatch] = useStateValue();
+  const updateCheckoutData = (key:string, val:string) => {
+    dispatch({
+      type: "UPDATE_CHECKOUT_DATA",
+      checkoutData: {
+        ...checkoutData,
+        [key]:val
+      }
+    });
+  }
   return (
     <div className="w-full p-1 px-2 rounded-lg flex flex-col">
     <div className="w-full flex flex-col mb-2">
       <label
-        htmlFor="network"
+        htmlFor="provider"
         className="font-bold text-sm mb-1 text-gray-100"
       >
         Select Provider
       </label>
       <select
-        id="network"
+        id="provider"
         className="w-full px-3 py-2 mb-1 border-2 text-white border-gray-500 rounded-md focus:outline-none focus:border-orange-500 focus:text-orange-500 bg-cartItem transition-colors"
+        onChange={(e) => updateCheckoutData("provider", e.target.value)}
       >
-        <option value="default" selected disabled>Select Network</option>
+        <option value="default" selected disabled>Select Provider</option>
         <option value="MTN">MTN Mobile Money</option>
         <option value="airtel">Airtel Tigo Money</option>
-        <option value="vodafone">Vodafone Cash</option>
+        <option value="Vodafone">Vodafone Cash</option>
 
       </select>
     </div>
@@ -49,6 +60,7 @@ const MomoForm = () => {
         className="w-full px-3 py-2 mb-1 border-2 text-white border-gray-500 rounded-md focus:outline-none focus:border-orange-500 focus:text-orange-500 bg-cartItem transition-colors"
         placeholder="Enter your number"
         autoComplete="off"
+        onChange = {(e) => updateCheckoutData("phone", e.target.value)}
       />
     </div>
   </div>
