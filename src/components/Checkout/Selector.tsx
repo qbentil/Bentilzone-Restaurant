a52d2a3d19c2766dc3112d7e4de7009a8df9ca10
qbentil <bentilshadrack72@gmail.com>
2022-07-17 10:41:27 +0000

    Dispatch selected PaymentMethod from selector

--- src/components/Checkout/Selector.tsx
+++ src/components/Checkout/Selector.tsx
@@ -1,49 +1,51 @@
+import { useStateValue } from "../../context/StateProvider";
 import { CreditCard, MOMO } from "../Assets";
-const Selector = ({
-  paymentMethod,
-  setPaymentMethod,
-}: {
-  paymentMethod: string;
-  setPaymentMethod: any;
-}) => {
+const Selector = () => {
+  const [{paymentMethod}, dispatch] = useStateValue();
+  const setPaymentMethod = (method:string) => {
+    dispatch({
+      type: "SET_PAYMENT_METHOD",
+      paymentMethod: method,
+    });
+  }
   return (
     <div className="my-3 flex w-full rounded-t-[2rem] justify-between p-3">
       <div
         className={`px-2 py-1 rounded-full flex items-center justify-center ${
-          paymentMethod === "momo" && "bg-white"
+          paymentMethod === "mobile_money" && "bg-white"
         }`}
       >
         <label
           htmlFor="type1"
           className="flex items-center cursor-pointer"
-          onClick={() => setPaymentMethod("momo")}
+          onClick={() => setPaymentMethod("mobile_money")}
         >
           <input
             type="radio"
             className="form-radio h-5 w-5 text-orange-500 cursor-pointer"
             name="type"
             id="type1"
-            checked={paymentMethod === "momo"}
+            checked={paymentMethod === "mobile_money"}
           />
           <img src={MOMO} alt="" className="h-8 ml-3" />
         </label>
       </div>
       <div
         className={`px-2 py-1 rounded-full flex items-center justify-center ${
-          paymentMethod === "card" && "bg-white"
+          paymentMethod === "bank" && "bg-white"
         }`}
       >
         <label
           htmlFor="type2"
           className="flex items-center cursor-pointer"
-          onClick={() => setPaymentMethod("card")}
+          onClick={() => setPaymentMethod("bank")}
         >
           <input
             type="radio"
             className="form-radio h-5 w-5 text-orange-500 cursor-pointer"
             name="type"
             id="type2"
-            checked={paymentMethod === "card"}
+            checked={paymentMethod === "bank"}
           />
           <img src={CreditCard} alt="" className="h-8 ml-3" />
         </label>
