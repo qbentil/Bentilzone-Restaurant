a00ee332917cdd9eac1be25ec85daa040c2c2b90
Bentil Shadrack <bentilshadrack72@gmail.com>
2022-10-10 17:57:01 +0000

    Create Menu Page

---
+++ src/Pages/Menu/index.tsx
@@ -0,0 +1,12 @@
+import { MenuSection } from "../../components";
+
+const Menu = () => {
+  return (
+    <div className='flex w-full h-auto flex-col items-center justify-center'>
+      <MenuSection title="Our Menu" />
+
+    </div>
+  );
+}
+
+export default Menu;
