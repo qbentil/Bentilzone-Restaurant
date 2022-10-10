67ad568e551b41e4dada0386eeeff5e1f2be6e95
Bentil Shadrack <bentilshadrack72@gmail.com>
2022-10-10 17:57:36 +0000

    Update menu

--- src/Pages/Menu/index.tsx
+++ src/Pages/Menu/index.tsx
@@ -3,7 +3,7 @@ import { MenuSection } from "../../components";
 const Menu = () => {
   return (
     <div className='flex w-full h-auto flex-col items-center justify-center'>
-      <MenuSection title="Our Menu" />
+      <MenuSection  />
 
     </div>
   );
