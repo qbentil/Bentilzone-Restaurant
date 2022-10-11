8f474c5b2d99650be85850da084ebf53f4d57458
Bentil Shadrack <bentilshadrack72@gmail.com>
2022-10-11 12:33:20 +0000

    Remopve unused imports

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -7,4 +7,3 @@ export {default as Profile} from './Profile'
 export {default as About} from './About';
 export {default as Services} from './Services';
 export {default as Menu} from './Menu';
-export {default as Contact} from './Contact';
