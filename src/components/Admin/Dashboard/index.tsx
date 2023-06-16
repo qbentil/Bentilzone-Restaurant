a9119d93ae4ed49f2e047687e30989557e509703
qbentil <bentilshadrack72@gmail.com>
2023-06-16 10:39:46 +0000

    Test Category Cards

--- src/components/Admin/Dashboard/index.tsx
+++ src/components/Admin/Dashboard/index.tsx
@@ -1,14 +1,37 @@
-import CategoryCards from "./CategoryCards";
+import Card from "./Card";
+import { Categories } from "../../../utils/categories"
+import { useStateValue } from "../../../context/StateProvider";
+import { FiGrid, FiShoppingBag, FiShoppingCart, FiUsers } from "react-icons/fi";
 
 const Dashboard = () => {
+  const [{ users, orders, foodItems }, dispatch] = useStateValue();
+  const data = [
+    {
+      title: "Users",
+      Icon: FiUsers,
+      count: users?.length || 0,
+    },
+    {
+      title: "Orders",
+      Icon: FiShoppingCart,
+      count: orders?.length || 0,
+    },
+    {
+      title: "Food Items",
+      Icon: FiShoppingBag,
+      count: foodItems?.length || 0,
+    },
+    {
+      title: "Categories",
+      Icon: FiGrid,
+      count: Categories?.length || 0,
+    }
+  ]
   return (
     <div className="w-full">
-        <div className="grid grid-cols-3 gap-4 p-2">
-            <CategoryCards />
-            <CategoryCards />
-            <CategoryCards />
-            <CategoryCards />
-        </div>
+      <div className="grid grid-cols-3 gap-4 p-2">
+        {data.map((item, index) => (<Card key={index} {...item} />))}
+      </div>
     </div>
   );
 };
